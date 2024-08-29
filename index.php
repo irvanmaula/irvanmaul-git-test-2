c2717752572550051567e2fcd52fb30ca7ff274f
irvanmaula <irvanmaul4321@gmail.com>
2024-08-29 10:00:37 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit nesciunt laborum quidem beatae similique, ea debitis aspernatur deserunt ullam doloremque minus eveniet expedita odit unde doloribus porro assumenda est. Eligendi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
